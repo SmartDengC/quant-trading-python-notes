30cde5c0bb347dc810452c4229fc694f9e4d6cd8
dengc4r <zero.dengc@outlook.com>
2026-06-02 23:51:12 +0800

    feat: 使用query_history_k_data()

--- code/query_history_k_data_5_minutes.py
+++ code/query_history_k_data_5_minutes.py
@@ -8,29 +8,31 @@ lg = bs.login()
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
 
-#### 获取沪深A股历史K线数据 ####
+# 获取沪深A股历史K线数据， 不包括港股
 # 详细指标参数，参见“历史行情指标参数”章节；“分钟线”参数与“日线”参数不同。“分钟线”不包含指数。
 # 分钟线指标：date,time,code,open,high,low,close,volume,amount,adjustflag
 # 周月线指标：date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg
-xiaomi_company_code = 'HK.01810'
-pufayinghang_company_code = 'sh.600000'
-rs = bs.query_history_k_data_plus(pufayinghang_company_code,
+sh600000 = 'sh.600000'
+sh600519 = 'sh.600519'  # 贵州茅台
+sz002475 = 'SZ.002475'  # 立讯精密
+
+rs = bs.query_history_k_data_plus(sz002475,
     "date,time,code,open,high,low,close,volume,amount,adjustflag",
     start_date='2022-07-01', end_date='2022-07-31',
     frequency="5", adjustflag="3")
 print('query_history_k_data_plus respond error_code:'+rs.error_code)
 print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
 
-#### 打印结果集 ####
+# 打印结果集
 data_list = []
 while (rs.error_code == '0') & rs.next():
     # 获取一条记录，将记录合并在一起
     data_list.append(rs.get_row_data())
 result = pd.DataFrame(data_list, columns=rs.fields)
 
-#### 结果集输出到csv文件 ####
-result.to_csv("./history_A_stock_k_data.csv", index=False)
+# 结果集输出到csv文件
+result.to_csv("../csv/history_A_stock_k_data_5_minutes.csv", index=False)
 display(result)
 
-#### 登出系统 ####
+# 登出系统
 bs.logout()
